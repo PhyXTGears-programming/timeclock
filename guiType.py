ee4468493d609bbf55b3d4666f1c9c503281c3a4
phyxtgearspro <phyxtgearspro@gmail.com>
2017-08-29 20:45:24 -0400

    add logo

--- guiType.py
+++ guiType.py
@@ -11,16 +11,18 @@ root = Tk()
 root.geometry('450x650')
 #root.resizeable(width=False,height=False)
 
-Label(root, text='PhyxtGears1720io').pack()
+logo1720 = PhotoImage(file='assets/logo.gif')
+Label(root, text='PhyxtGears1720io', font='Courier 8').pack()
+Label(root, image=logo1720).pack()
 
 framelist = Frame(root)
 scrolbar = Scrollbar(framelist, orient=VERTICAL)
 namelist = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 8')
 iolist   = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 8', justify=CENTER)
 
-namelist.config(width=50,height=12)
-iolist.config(width=2,height=12)
-scrolbar.config(command=setScroll)
+namelist.config(width=50,height=18)
+iolist.config(width=2,height=18)
+scrolbar.config(command=setScroll, width=42)
 
 scrolbar.pack(side=RIGHT, fill=Y)
 namelist.pack(side=LEFT, fill=BOTH, expand=1)
@@ -28,9 +30,10 @@ iolist.pack(side=LEFT, fill=BOTH, expand=1)
 
 framelist.pack()
 
-iobutton = Button(root, text='IO', command=onSelect, width=20, height=2)
+iobutton = Button(root, text='IO', font='Courier 8', command=onSelect, width=20, height=2)
 iobutton.pack()
 
+namelist.insert(END, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!'); iolist.insert(END, 'o')
 for x in range(100):
 	namelist.insert(END, str(x))
 	if random.random() >= 0.5:
