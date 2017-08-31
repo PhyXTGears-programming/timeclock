6585732d49ba551820061b2728ab895e9de56b88
phyxtgearspro <phyxtgearspro@gmail.com>
2017-08-31 19:53:33 -0400

    load names from name.txt

--- guiType.py
+++ guiType.py
@@ -2,6 +2,8 @@ import random
 import os
 from tkinter import *
 
+from opts import *
+
 def setScroll(*args):
 	namelist.yview(*args)
 	iolist.yview(*args)
@@ -12,17 +14,17 @@ root.geometry('450x650')
 #root.resizeable(width=False,height=False)
 
 logo1720 = PhotoImage(file='assets/logo.gif')
-Label(root, text='PhyxtGears1720io', font='Courier 8').pack()
+Label(root, text='PhyxtGears1720io', font='Courier 12').pack()
 Label(root, image=logo1720).pack()
 
 framelist = Frame(root)
 scrolbar = Scrollbar(framelist, orient=VERTICAL)
-namelist = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 8')
-iolist   = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 8', justify=CENTER)
+namelist = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 12')
+iolist   = Listbox(framelist, yscrollcommand=scrolbar.set, font='Courier 12', justify=CENTER)
 
-namelist.config(width=50,height=18)
-iolist.config(width=2,height=18)
-scrolbar.config(command=setScroll, width=42)
+namelist.config(width=38,height=18)
+iolist.config(width=1,height=18)
+scrolbar.config(command=setScroll, width=52)
 
 scrolbar.pack(side=RIGHT, fill=Y)
 namelist.pack(side=LEFT, fill=BOTH, expand=1)
@@ -30,16 +32,24 @@ iolist.pack(side=LEFT, fill=BOTH, expand=1)
 
 framelist.pack()
 
-iobutton = Button(root, text='IO', font='Courier 8', command=onSelect, width=20, height=2)
+iobutton = Button(root, text='IO', font='Courier 12', command=onSelect, width=16, height=2)
 iobutton.pack()
 
-namelist.insert(END, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!'); iolist.insert(END, 'o')
+'''namelist.insert(END, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!'); iolist.insert(END, 'o')
 for x in range(100):
 	namelist.insert(END, str(x))
 	if random.random() >= 0.5:
 		iolist.insert(END, 'o')
 	else:
-		iolist.insert(END, 'i')
+		iolist.insert(END, 'i')'''
+for x in range(10):
+	for line in open(opts['name.txt']):
+		line = line.strip().split('|')
+		namelist.insert(END, line[0])
+		if random.random() >= 0.5:
+			iolist.insert(END, 'o')
+		else:
+			iolist.insert(END, 'i')
 
 root.mainloop()
 
