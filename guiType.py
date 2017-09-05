4479d26dbf6e8dae395a1650cd2193c66c15200f
phyxtgearspro <phyxtgearspro@gmail.com>
2017-09-05 19:59:57 -0400

    small adjustments + prep for err label

--- guiType.py
+++ guiType.py
@@ -20,7 +20,6 @@ for line in open('opts.txt'): # load options
 
 open(opts['usernameFile'],'a+').close()
 
-#from opts import *
 
 def setScroll(*args):
 	namelist.yview(*args)
@@ -29,55 +28,63 @@ def getNameFromListbox(): print(namelist.get(namelist.curselection()[0])) # retu
 
 def makeNewUserWindow():
 	nuWin = Toplevel(root)
-	nuWin.geometry('420x300')
+	nuWin.geometry('420x200')
 	
-	Label(nuWin, text='Fullname: ', font='Courier 12').grid(sticky=E,padx=2,pady=2)
-	Label(nuWin, text='Username: ', font='Courier 12').grid(sticky=E,padx=2,pady=2)
+	inputframe = Frame(nuWin)
+	butonframe = Frame(nuWin)
 	
-	fullnameEntry = Entry(nuWin, font='Courier 12', width=30)
-	usernameEntry = Entry(nuWin, font='Courier 12', width=30)
+	Label(inputframe, text='Fullname: ', font='Courier 12').grid(sticky=E,padx=2,pady=2)
+	Label(inputframe, text='Username: ', font='Courier 12').grid(sticky=E,padx=2,pady=2)
+	
+	fullnameEntry = Entry(inputframe, font='Courier 12', width=30)
+	usernameEntry = Entry(inputframe, font='Courier 12', width=30)
 	fullnameEntry.grid(row=0,column=1)
 	usernameEntry.grid(row=1,column=1)
+	inputframe.pack()
+	
+	errorLabel = Label(nuWin, font='Courier 12', text='1 line\n2 line', fg='red')
+	errorLabel.pack()
+	
+	finishButton = Button(butonframe, text='Create User',font='Courier 12', width=16)
+	cancelButton = Button(butonframe, text='Cancel',     font='Courier 12', width=16)
+	finishButton.grid(row=0,column=1)
+	cancelButton.grid(row=0,column=0)
+	butonframe.pack()
 	
 
-def maingui():
-	framelist = Frame(root)
-	scrolbar = Scrollbar(framelist, orient=VERTICAL)
-	namelist = Listbox(framelist, selectmode=SINGLE, yscrollcommand=scrolbar.set, font='Courier 12')
-	iolist   = Listbox(framelist, selectmode=SINGLE, yscrollcommand=scrolbar.set, font='Courier 12', justify=CENTER)
+framelist = Frame(root)
+scrolbar = Scrollbar(framelist, orient=VERTICAL)
+namelist = Listbox(framelist, selectmode=SINGLE, yscrollcommand=scrolbar.set, font='Courier 12')
+iolist   = Listbox(framelist, selectmode=SINGLE, yscrollcommand=scrolbar.set, font='Courier 12', justify=CENTER)
 
-	namelist.config(width=38,height=18)
-	iolist.config(width=1,height=18)
-	scrolbar.config(command=setScroll, width=52)
+namelist.config(width=38,height=18)
+iolist.config(width=1,height=18)
+scrolbar.config(command=setScroll, width=52)
 
-	scrolbar.pack(side=RIGHT, fill=Y)
-	namelist.pack(side=LEFT, fill=BOTH, expand=1)
-	iolist.pack(side=LEFT, fill=BOTH, expand=1)
-	framelist.pack(side=LEFT)
+scrolbar.pack(side=RIGHT, fill=Y)
+namelist.pack(side=LEFT, fill=BOTH, expand=1)
+iolist.pack(side=LEFT, fill=BOTH, expand=1)
+framelist.pack(side=LEFT)
 
-	logo1720 = PhotoImage(file='assets/logo.gif')
-	Label(root, text='PhyxtGears1720io', font='Courier 12').pack()
-	Label(root, image=logo1720).pack()
+logo1720 = PhotoImage(file='assets/logo.gif')
+Label(root, text='PhyxtGears1720io', font='Courier 12').pack()
+Label(root, image=logo1720).pack()
 
-	frameio = Frame(root)
-	innbutton = Button(frameio, text='IN',  font='Courier 12', command=getNameFromListbox, width=16, height=2)
-	outbutton = Button(frameio, text='OUT', font='Courier 12', command=getNameFromListbox, width=16, height=2)
-	newbutton = Button(frameio, text='New User', font='Courier 12', command=makeNewUserWindow, width=16, height=2)
-	innbutton.grid(row=0,pady=6)
-	outbutton.grid(row=1,pady=6)
-	newbutton.grid(row=2,pady=6)
-	frameio.pack()
+frameio = Frame(root)
+innbutton = Button(frameio, text='IN',  font='Courier 12', command=getNameFromListbox, width=16, height=2)
+outbutton = Button(frameio, text='OUT', font='Courier 12', command=getNameFromListbox, width=16, height=2)
+newbutton = Button(frameio, text='New User', font='Courier 12', command=makeNewUserWindow, width=16, height=2)
+innbutton.grid(row=0,pady=6)
+outbutton.grid(row=1,pady=6)
+newbutton.grid(row=2,pady=6)
+frameio.pack()
 
-	for line in open(opts['usernameFile']):
-		line = line.strip().split('|')
-		namelist.insert(END, line[0])
-		if random.random() >= 0.5:
-			iolist.insert(END, 'o')
-		else:
-			iolist.insert(END, 'i')
+for line in open(opts['usernameFile']):
+	line = line.strip().split('|')
+	namelist.insert(END, line[0])
+	iolist.insert(END, 'N') #if random.random() >= 0.5: iolist.insert(END, 'o') else: iolist.insert(END, 'i')
 
-	root.mainloop()
+root.mainloop()
 
-maingui()
-
+#maingui()
 #if __name__=='__main__': main()
