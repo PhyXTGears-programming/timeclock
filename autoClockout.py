ca120884e75e8405faee173d12927e220b046f1c
PhyXTGears 1720 <phyxtgearspro@gmail.com>
2017-09-28 20:09:16 -0400

    autoclockout works!

--- autoClockout.py
+++ autoClockout.py
@@ -10,10 +10,12 @@ for line in open('opts.txt'): # load options
 def main():
 	while True:
 		if time.strftime("%H:%M:%S")==opts['autoClockOut']:
-			for item in os.listdir(path=opts['path']):
+			print('doin it')
+			for item in os.listdir(path=opts['pathTime']):
 				io = []
-				with open(opts['path']+item) as i: io = i.readlines()[-1].split()
+				with open(opts['pathTime']+item) as i: io = i.readlines()[-1].split()
 				if io[0] == 'i':
 					io[0] = 'o'
-					with open(opts['path']+item, 'a') as i: i.write(' '.join(io)+'\n')
+					with open(opts['pathTime']+item, 'a') as i: i.write(' '.join(io)+' a\n')
+			print('done')
 			time.sleep(1)
