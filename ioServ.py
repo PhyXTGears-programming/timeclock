c0fee85b8fe32f5612797b52e86d476c2c96dd05
PhyXTGears 1720 <phyxtgearspro@gmail.com>
2017-10-26 20:57:22 -0400

    remove some options stuff

--- ioServ.py
+++ ioServ.py
@@ -25,9 +25,7 @@ def generateDefaultOpts():
 	autoClockOut : 00:00:00 # time to automatically sign out everyone signed in (00:00:00 is midnight) 
 	autoClockLim : 05:00:00 # If the person signs out before this time, the auto clock out is not counted 
 	usernameFile : usernameFile.txt # file to save usernames in 
-	adminPass : 1234 # passcode used to shut down program when it is full screen 
-	hoursLocation : enterLoationOf_timeclock-hours # location of PhyXTGears-programming/timeclock library 
-	# If you leave hoursLocation the same, most time calculation functions will be disabled 
+	adminPass : 1234 # passcode used to shut down program when it is full screen
 	"""
 	os.chdir(os.path.dirname(__file__))
 	#input("Press enter to overwrite current options file with the default") 
